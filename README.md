58dad360a7e80619d320bcd0cfa0aed6bc456a53
Adam Baker <adam.baker97202@gmail.com>
2016-10-21 16:12:52 -0700

    problems

---
+++ README.md
@@ -0,0 +1,3 @@
+I'm not really sure why this is not working right now, but it isn't.
+
+The values won't display.
---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+    event.preventDefault();
+  debugger;
+
+    var rubyRails = "Ruby on Rails";
+    var css = "CSS";
+    var cSharp = "C Sharp";
+
+
+    var tracker = 0;
+    var middle = 0;
+
+    var businessType = $("input:radio[name=businessType]:checked").val();
+    var codingType = $("input:radio[name=codingType]:checked").val();
+    var endType = $("input:radio[name=endType]:checked").val();
+    var webType = $("input:radio[name=webType]:checked").val();
+    var interType = $("input:radio[name=interType]:checked)".val());
+
+
+
+// question 1
+
+    if (businessType === "agree") {
+      tracker++;
+    }
+    else {
+      tracker--;
+    }
+// question 2
+    if (codingType === "agree") {
+      tracker++;
+    }
+    else {
+      tracker--;
+    }
+// question 3
+    if (endType === "agree") {
+      tracker++;
+    }
+    else {
+      tracker--;
+    }
+// question 4
+    if (webType === "agree") {
+      tracker++;
+    }
+    else {
+      tracker--;
+    }
+//question 5
+    if (interType === "agree") {
+      tracker ++;
+    }
+    else {
+      tracker --;
+    }
+
+// end of business logic
+
+
+
+  if (tracker === middle) {
+    $(".result").text("You should take any track");
+  }
+  else if (tracker < -2) {
+    $(".result").text("You should take " + rubyRails + ".");
+  }
+  else if (tracker > 2) {
+    $(".result").text("You should take " + css + ".");
+  }
+  else
+    $(".result").text("You should take " + cSharp "."));
+
+
+
+});
+});
